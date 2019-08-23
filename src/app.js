2524fcd95653137c640e71065d5751325955c853
Rohan.choudhary <rohan.choudhary@99acres.com>
2019-08-23 10:08:38 +0530

    MERGE : done with basic structure of login module

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const {registerNewUser , authenticateUser} = require('../src/utility/auth');
-const {checkAuthentication} = require('./utility/middleWare');
 app.use(bodyParser.urlencoded({ extended: false })) 
 
 // parse application/json
@@ -17,17 +16,23 @@ app.use(express.json());
  * 
  */
 
-app.use((req,res)=>{
+app.use((req,res,next)=>{
    let emailId = req.body.email_Id;
     let password = req.body.password;
-    checkAuthentication(emailId,password); 
+     authenticateUser(emailId , password)
+     .then(response=>{
+         response.value == true ? res.status(200).json({message:"user is present"}) : next();
+     })
+     .catch(err=>{
+         console.error("!Invalid Query ",err);
+     })
 })
 app.post("/auth-checking",(req,res)=>{
     let emailId = req.body.email_Id;
     let password = req.body.password;
     registerNewUser(emailId , password)
     .then(response=>{
-        console.log(response);
+        if(response.status == 200)res.status(200).json({message: 'user created successfully'}) 
     })
     .catch(err=>{
         console.log(err);
