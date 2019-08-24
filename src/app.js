50b44210f2b9809ae4c9ef1a20b76c5d42270a4d
Rohan.choudhary <rohan.choudhary@99acres.com>
2019-08-24 11:46:56 +0530

    MERGE : Login module done

--- src/app.js
+++ src/app.js
@@ -27,17 +27,28 @@ app.use((req,res,next)=>{
          console.error("!Invalid Query ",err);
      })
 })
-app.post("/auth-checking",(req,res)=>{
+app.use((req,res,next)=>{
     let emailId = req.body.email_Id;
     let password = req.body.password;
     registerNewUser(emailId , password)
     .then(response=>{
-        if(response.status == 200)res.status(200).json({message: 'user created successfully'}) 
+        response.status == 200 ? res.status(200).json({message: 'user created successfully'}) : next(); 
     })
     .catch(err=>{
         console.log(err);
     })  
 })
+
+app.use((error, req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message:error.message
+        }
+    })
+})
+
+
 
 module.exports = {app
 };
